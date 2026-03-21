156ac009b080f93dbf265494f110359fa0063298
Abhay-Rwt <abhithrckstr90@gmail.com>
2026-03-21 12:16:43 +0530

    Fixed cors logic

--- server/index.js
+++ server/index.js
@@ -24,7 +24,11 @@ database.connect();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://studynotion-frontend-one-ashy.vercel.app/",
+    origin: [
+        "http://localhost:3000",
+        "https://studynotion-frontend-one-ashy.vercel.app/",
+        "https://studynotion-frontend-abhay-rawats-projects-2e795baf.vercel.app/"
+    ],
     credentials: true
 }));
 app.use(
