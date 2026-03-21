0e0f6d7d32ec54ec9c90fa13d7b1a71dba2cbc68
Abhay-Rwt <abhithrckstr90@gmail.com>
2026-03-21 14:54:50 +0530

    fixed authentication problem

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -6,7 +6,7 @@ const User = require('../models/User')
 // auth
 exports.auth = async (req, res, next) => {
     try{
-        const token = req.cookies.token || req.body.token || req.header("Authorisation").replace("Bearer ", "");
+        const token = req.cookies.token || req.body.token || req.header("Authorization").replace("Bearer ", "");
         
         if(!token){
             return res.status(401).json({
